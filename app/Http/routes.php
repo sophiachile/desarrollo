1507060237b6d3ba3a81e7722d9e1e3127d43f8e
crgg1985 <carlos.gonzalez.gavilan@gmail.com>
2016-08-27 20:23:44 -0300

    saco ruta prueba

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,8 +18,6 @@ Route::get('muro', 'ProductController@muro');
 
 Route::get('home', 'HomeController@index');
 
-Route::get('prueba', 'ProductController@prueba');
-
 Route::get('home', 'HomeController@index');
 
 Route::get('welcome', function () {
