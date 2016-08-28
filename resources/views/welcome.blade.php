7e56ca0cde4e9100c5855f2a6215312a2c16f713
jaimeHuenchun <jaime.lhj@gmail.com>
2016-08-27 20:40:07 -0400

    pequeña modificacion en pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,62 @@
-<html>
-	<head>
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+@extends('layout.master')
+@section('content')
+<article class="container">
+		<div class="row">
+			<div class="col-sm-10">
+			  <div class="panel panel-default">
+ 				 <div class="panel-body">
+				   <h1><strong> Bienvenido a Sophia</					strong></h1>
+                   <h4>Para poder finalizar su registro es necesario que porfavor complete los siguientes campos: </h4>
+                   <form role="form">
+  <div class="form-group">
+    <label for="tipo-insitucion">Seleccione tipo de Institucion</label>
+ 	 	<div > 
+   			<select name="select-tipoInstitucion" class="form-control" >
+ 			 <option>1</option>
+          	<option>2</option>
+ 			 <option>3</option>
+ 			 <option>4</option>
+		     <option>5</option>
+			</select>
+  		</div>
+  </div>
+  
+  <div class="form-group">
+    <label for="nom-institucion">Seleccione Institucion</label>
+    <div > 
+   			<select name="select-nomInstitucion" class="form-control" >
+ 			 <option>1</option>
+          	<option>2</option>
+ 			 <option>3</option>
+ 			 <option>4</option>
+		     <option>5</option>
+			</select>
+  		</div>
+  </div>
+  
+    <div class="form-group">
+    <label for="nom-carrera">Seleccione su Carrera</label>
+    <div > 
+   			<select name="select-nomCarrera" class="form-control" >
+ 			 <option>1</option>
+          	<option>2</option>
+ 			 <option>3</option>
+ 			 <option>4</option>
+		     <option>5</option>
+			</select>
+  		</div>
+  </div>
+  
+  <button type="submit" class="btn btn-default">Enviar</button>
+					</form>
+		        </div>
+			  </div>
 			</div>
 		</div>
-	</body>
-</html>
+</article>
+
+
+
+
+
+@stop()
